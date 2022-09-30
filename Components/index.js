abd3902f2e602a6e002e1a2f67329fdc1535dd7a
spore12 <109874776+spore12@users.noreply.github.com>
2022-09-30 22:35:19 +0200

    Update index.js

--- Components/index.js
+++ Components/index.js
@@ -1,10 +1,52 @@
 const Discord = require("discord.js")
 const client = new Discord.Client(
-    { intents: ["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"] }
+    { intents: ["Guilds", "GuildMembers", "GuildMessages", "GuildEmojisAndStickers"] }
 )
 
-client.login("MTAxMzc4MjUwMzgyMTE2MDU1OQ.GhRvS4.BKhcikHqS4Rkr-hypSrlmBaL1SQqhRuNAzaUQQ")
+client.login("no token? :((")
 
 client.on("ready", () => {
-    console.log("Ce Sto")
+    console.log("SBOTTELLO ONLINE :)")
 })
+
+client.on("messageCreate", () => {
+    console.log("a message got sent")
+})
+
+client.on("messageDelete", () => {
+    console.log("a message got deleted")
+})
+
+client.on("emojiCreate",  () => {
+    console.log("an emoji got created")
+})
+
+client.on("emojiDelete", () => {
+    console.log("an emoji got deleted")
+})
+
+client.on("channelCreate", () => {
+    console.log("a channel got created")
+})
+
+client.on("channelDelete", () => {
+    console.log("a channel got deleted")
+})
+
+client.on("roleCreate", () => {
+    console.log("a role got created")
+})
+
+client.on("roleDelete", () => {
+    console.log("a role got deleted")
+})
+
+client.on("roleUpdate", () => {
+    console.log("a role got updated")
+})
+
+client.off("warn", () => {
+    console.log("SBOTTELLO IS GOING OFFLINE :(")           ///// da sistemare/have to repair /////
+})
+
+
